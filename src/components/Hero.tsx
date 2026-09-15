f287620a97f28398e51c571bbf77431ae4cb14dc
meghnakyatham <meghnakyatham05@gmail.com>
2026-09-15 19:39:50 +0530

    Update website

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
       y: 0, 
       opacity: 1, 
       filter: 'blur(0px)',
-      transition: { duration: 1.2, ease: [0.16, 1, 0.3, 1] }
+      transition: { duration: 1.2, ease: [0.16, 1, 0.3, 1] as [number, number, number, number] }
     }
   };
 
